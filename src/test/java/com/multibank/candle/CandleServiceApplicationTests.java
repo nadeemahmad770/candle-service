2aa69d7f7b31f7246127f7c92d543b9c2698bea5
Nadeem Ahmad <nadeemahmad770@gmail.com>
2026-02-28 19:45:02 +0530

    service initial code with pom entries

---
+++ src/test/java/com/multibank/candle/CandleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multibank.candle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
